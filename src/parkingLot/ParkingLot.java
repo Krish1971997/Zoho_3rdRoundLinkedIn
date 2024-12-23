c49ea32c93f5ae0292ff148004e57e4a5a47c3ed
Krish1971997 <krishnakumar1971997@gmail.com>
2024-12-23 22:01:46 +0530

    first commit

--- src/parkingLot/ParkingLot.java
+++ src/parkingLot/ParkingLot.java
@@ -4,45 +4,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ParkingLot {
-    private static ParkingLot instance;
-    private List<Level> levels;
+	private static ParkingLot instance;
+	private List<Level> levels;
 
-    private ParkingLot() {
-        levels = new ArrayList<>();
-    }
+	private ParkingLot() {
+		levels = new ArrayList<>();
+	}
 
-    public static synchronized ParkingLot getInstance() {
-        if (instance == null) {
-            instance = new ParkingLot();
-        }
-        return instance;
-    }
+	public static synchronized ParkingLot getInstance() {
+		if (instance == null) {
+			instance = new ParkingLot();
+		}
+		return instance;
+	}
 
-    public void addLevel(Level level) {
-        levels.add(level);
-    }
+	public void addLevel(Level level) {
+		levels.add(level);
+	}
 
-    public boolean parkVehicle(Vehicle vehicle) {
-        for (Level level : levels) {
-            if (level.parkVehicle(vehicle)) {
-                return true;
-            }
-        }
-        return false;
-    }
+	public boolean parkVehicle(Vehicle vehicle) {
+		for (Level level : levels) {
+			if (level.parkVehicle(vehicle)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    public boolean unParkVehicle(Vehicle vehicle) {
-        for (Level level : levels) {
-            if (level.unParkVehicle(vehicle)) {
-                return true;
-            }
-        }
-        return false;
-    }
+	public boolean unParkVehicle(Vehicle vehicle) {
+		for (Level level : levels) {
+			if (level.unParkVehicle(vehicle))
+				return true;
+		}
+		return false;
+	}
 
-    public void displayAvailability() {
-        for (Level level : levels) {
-            level.displayAvailability();
-        }
-    }
+	public void displayAvailability() {
+		for (Level level : levels) {
+			level.displayAvailability();
+		}
+	}
 }
